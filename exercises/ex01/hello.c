9d03b1dd5bd79ed59304fbcfcbda0dac20e57ffd
NickShermeister <ncsherman159@gmail.com>
2019-01-29 15:06:57 -0500

    Tried to neaten up answers al ittle bit.

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -9,14 +9,14 @@ A1:subq	$16, %rsp
 movl	$5, -4(%rbp)
 (lines 16 & 17)
 Q2: What happens if you turn on optimization using the flag -O2?
-A2: The code seems to be split up into multiple (functions? simultaneous calls?) dictated by the .###:
+A2: The code seems to be split up into multiple (functions? simultaneous calls?) dictated by the .###. It also just puts the values directly into the assembly code.
 Q3: Modify the printf statement to print x, then compile it with and without optimization. What effect does it have when you print x?
 A3:Once more, the code is split up into smaller functions, presumably so they can run simultaneously.
 Q4: Compile with and without optimization. What happens to x and y?
 The parallelization is still here. The what y equals is put directly into the print statement (instead of calculating y separately then using the value.)
 What conclusions can you draw about how optimization works?
 Optimization is useful for parallelization and for combining assignments and reusage of variables into smaller spaces.
-A4:
+There is also compilation prediction that allows for shortened code by precomputing x because it isn't used.
 
 
 */

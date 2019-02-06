54aea597297f49e50a75070835210d5e399469c8
NickShermeister <ncsherman159@gmail.com>
2019-02-06 14:22:32 -0500

    First turn-in of hw2; have one section left.

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -10,17 +10,98 @@ License: GNU GPLv3
 
 int var1;
 
+/*
+Compile and run aspace.c in this directory.
+
+Read the code. You might want to read about malloc here.
+-done
+
+Based on the output, draw a sketch of the virtual address space (see page 80 of HFC). Show the relative locations of the stack, heap, globals, constants, and code.
+-see address_space.png
+
+Add a second call to malloc and check whether the heap on your system grows up (toward larger addresses).
+-see address_space.png
+
+Add a function that prints the address of a local variable, and check whether the stack grows down.
+-see below
+Choose a random number between 1 and 32, and allocate two chunks with that size.
+How much space is there between them? Hint: Google knows how to subtract hexadecimal numbers.
+see below.
+*/
+
+
+/*
+5:
+  Function which establishes whether the stack is growing up or down.
+  Inputs:
+  int*varIn: The first variable defined in the stack.
+
+  Returns a boolean. 0 if down, 1 if up.
+*/
+int stackGrowth (int varIn) {
+  printf("The input variable varIn points to %p\n", &varIn);
+  int varDef = 10;
+  printf("The variable defined in-function points to %p\n", &varDef);
+  if(&varIn < &varDef){ //If the 1st location is lower in memory than the second
+    printf("The stack is growing up.\n");
+    return 1;
+  }
+  else {
+    printf("The stack is growing down.\n");
+    return 0;
+  }
+}
+
+/*
+#6:
+Random numbers chosen: 20, 1, 31
+20 - 32
+1 - 32
+31 - 48???? (shouldn't this be 32???)
+30 - 48
+24 - 32
+27 - 48
+25 -48
+
+Why is this happening iin this pattern? That doesn't make sense... 12 in malloc = 16 bytes?
+*/
+void sizeDiff(){
+  void *temp1 = malloc(25);
+  void *temp2 = malloc(25);
+
+  printf ("temp1 points to %p\n", temp1);
+  printf ("temp2  points to %p\n", temp2);
+  int diff = temp2 - temp1; //We know the stack grows up on the test computer.
+
+  printf("The difference between temp1 and temp2 is: %d", diff);
+}
+
 int main ()
 {
     int var2 = 5;
     void *p = malloc(128);
+    void *p2 = malloc(128);
     char *s = "Hello, World";
 
     printf ("Address of main is %p\n", main);
     printf ("Address of var1 is %p\n", &var1);
     printf ("Address of var2 is %p\n", &var2);
+    /*
+    Output from run including var2:
+    Address of main is 0x4005d6
+    Address of var1 is 0x60104c
+    Address of var2 is 0x7ffd7bdf149c
+    p points to 0x1a70010
+    p2 points to 0x1a700a0
+    s points to 0x400744
+
+    */
+    int temp = stackGrowth(var2);
     printf ("p points to %p\n", p);
+    printf ("p2 points to %p\n", p2);
     printf ("s points to %p\n", s);
+
+    sizeDiff();
 
     return 0;
 }
--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -5,40 +5,88 @@ License: GNU GPLv3
 
 */
 
+/*
+Read stack.c, which should be in this directory. What is it intended to do? What would the output be if the program worked as expected?
+stack.c is intended to create an array of size SIZE (which is a constant currently set to 5) of ints. These values are then set to 42. As it stands, array bar does nothing except create an array of size SIZE valued 0 to size-1. It then is supposed to print out the first generated array, which was passed back to the main function.
+If it worked as expected, it should print 42 five times.
+
+
+Compile it. Do you get a warning? What does it mean?
+-I got a warning that stated:
+warning: function returns address of local variable [-Wreturn-local-addr]
+This means that the variable which we are trying to pass back and forth is being deleted from the stack as it is a local variable and therefore we will lose all references to it, which is why it shouldn't be returned.
+
+Run it. What happens? You might get a segmentation fault, or you might get some strange output. Can you explain it? You might find it helpful to draw a stack diagram.
+After running it, I get two printed addresses of the same value (which was due to foo and bar taking up the same amount of space, causing array to be located at the same disk space in both foo and bar, resulting in the same location being printed in each function)
+and then a segmentation fault. This occurs because the pointer that was array was still passed to the main function, but the area it pointed to we were no longer able to look at.
+We ended up with a segmentation fault due to this fact.
+
+
+Comment out the print statements in foo() and bar() and run it again. What happens now?
+With -Wall on we get some more errors! Specifically, we're we're still warned about returning array in foo but in bar we're warned that array is set but not used for some reason!
+We also still get a segmentation fault when the main function tries to read at the old location of array.
+
+Add comments to the code to explain these experiments and the results, then check the code in to the repo.
+-done
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #define SIZE 5
 
+/*
+Function that is supposed to create an array of size SIZE (constant, 5) and then set all of its values to 42.
+Currently, the function creates array as a local variable and therefore it is lost.
+returns int*: the location of where array should be.
+*/
 int *foo() {
+  //Declaration of local variables
     int i;
     int array[SIZE];
 
-    printf("%p\n", array);
+    // printf("%p\n", array);
 
+    //Looped assignment of array variable values
     for (i=0; i<SIZE; i++) {
         array[i] = 42;
     }
+    //Return the pointer to the array that is to be deleted.
     return array;
 }
-
+/*
+Function that locally creates an array of size SIZE (constant, 5) and then instantiates the diferent locations' values to the locaiton (e.g. the 0th slot is 0).
+Currently works, but doesn't do anything.
+*/
 void bar() {
+  //Declaration of variables
     int i;
     int array[SIZE];
 
-    printf("%p\n", array);
+    // printf("%p\n", array);
 
+    //Instanitation loop
     for (i=0; i<SIZE; i++) {
         array[i] = i;
     }
 }
 
+/*
+Main function which is supposed to take an array made in foo and print out the values.
+
+In actuality, foo makes a local variable not a global variable and therefore the variable is inacessible to the main function.
+*/
 int main()
 {
+    //Declar variables
     int i;
+    //Make the critical mistake of not having a global array.
     int *array = foo();
+    //Be confused as to why this function exists. The devs are uncertain as well.
     bar();
 
+    //Recursively print the array if we have access to it. Otherwise, have a segmentation fault.
     for (i=0; i<SIZE; i++) {
         printf("%d\n", array[i]);
     }

b0569196f08a73341e1b244628294b13559a4fb3
NickShermeister <ncsherman159@gmail.com>
2019-04-05 23:44:41 -0400

    Needs more error handling, but provisionally done with ex08.

---
+++ exercises/ex08/word_count.c
@@ -0,0 +1,106 @@
+/* Simple word counting completed for Software Systems 2019 at Olin College.
+Creator: Nicholas Sherman
+
+*/
+#include <glib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define BUFFERSIZE 1024
+
+void iterator(gpointer key, gpointer value, gpointer user_data) {
+  // printf("Size of key: %d\n", sizeof(key));
+  // printf("keyval: %d\n", key);
+  char* tempkey = (char*) key;
+  // printf("tempkey: %s\n", tempkey);
+ printf(user_data, key, value);
+}
+
+int main(int argc, char** argv) {
+
+  int i;
+  //Make sure that we're starting at the right arg; we know this based on what we take in (every valid arg increments this).
+  int start = 1;
+  int append = 0;
+  int opt;
+  FILE *out = fopen("answers.txt", "r");
+
+  while((opt = getopt(argc, argv, "f")) != -1)
+  {
+      switch(opt)
+      {
+          case 'f':
+            start++;
+            out = fopen(argv[start], "r");
+            break;
+          default:
+            printf("That is not a valid input arguments. Refer to -h for what inputs are valid. Input was: %c\n", opt);
+            return -1;
+      }
+  }
+
+  char x[BUFFERSIZE]; //in case there are some really long words
+  char y[BUFFERSIZE];
+  char * pointer1;
+  char * pointer2;
+
+  GHashTable* hash = g_hash_table_new(g_str_hash, g_str_equal);
+  int temp;
+
+  while (fscanf(out, " %1023s", x) == 1) {
+    temp = 1;
+    pointer1 = &x[0];
+    pointer2 = &y[0];
+    // puts(x);
+    while (*pointer1)
+    {
+       if (ispunct(*pointer1))
+       {
+          // Skip because [punct]
+          pointer1++;
+          // printf("Is punctuation!: %c\n", *pointer1);
+       }
+       else if (isupper(*pointer1))
+       {
+          // All lowercase is probably better
+          *pointer2 = tolower(*pointer1);
+          pointer2++;
+          pointer1++;
+       }
+       else
+       {
+          // Copy character
+          // printf("Is not punctuation! %c\n", *pointer1);
+          *pointer2 = *pointer1;
+          pointer1++;
+          pointer2++;
+       }
+    }
+
+    *pointer2 = 0;
+    // puts(y);
+
+    if(g_hash_table_contains(hash, y)){
+      temp = g_hash_table_lookup(hash, y);
+      g_hash_table_replace(hash, g_strdup(y), (temp + 1));
+    }
+    else {
+       g_hash_table_insert(hash, g_strdup(y), temp);
+    }
+
+    memset(x, 0, BUFFERSIZE);
+    memset(y, 0, BUFFERSIZE);
+
+  }
+
+  g_hash_table_foreach(hash, (GHFunc)iterator, "The number of occurances of %s is %d\n");
+
+
+ g_hash_table_destroy(hash);
+ fclose(out);
+
+
+ return 0;
+}
